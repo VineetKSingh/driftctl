54a1deeda0a4f402b88559cb42b880ce3bc1aeb3
sundowndev <raphael@crvx.fr>
2021-08-31 10:59:45 +0200

    refactor: improve error logs

--- pkg/cmd/scan/output/config.go
+++ pkg/cmd/scan/output/config.go
@@ -1,6 +1,12 @@
 package output
 
+import "fmt"
+
 type OutputConfig struct {
 	Key     string
 	Options map[string]string
+}
+
+func (o *OutputConfig) String() string {
+	return fmt.Sprintf("%s://%s", o.Key, o.Options["path"])
 }
